360e640855e1a519bddc017a63b7b61d581f208b
mathew_francis <mathewfvv@gmail.com>
2023-08-11 12:23:06 +0530

    FirstCommit

---
+++ src/test/java/org/example/tests/ApplicationTests.java
@@ -0,0 +1,22 @@
+package org.example.tests;
+
+
+import junit.framework.Assert;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTests {
+
+    @BeforeAll
+    public static void beforeAllTheMethodRun(){
+
+        System.out.println("Before Start clean");
+
+    }
+
+    @Test
+    public void tester(){
+        Assert.assertEquals("Hello","Hello");
+    }
+
+}

d60eda8e95e37acf8119d0c9c7f589966f5b3d02
mathew_francis <mathewfvv@gmail.com>
2023-08-11 13:09:32 +0530

    Second commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,10 +4,25 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        final double policyAmount = 100_000_000;
+        final int beneficiaries = 3;
+        float percentageFloat = 1.0f / beneficiaries;
+        double percentage = 1.0 / beneficiaries;
+
+
+        BigDecimal bigDecimal = new BigDecimal();
+
+        System.out.printf("Payout = %,.2f%n", bigDecimal.percentageFloatReturnAmount(policyAmount,percentageFloat));
+
+        System.out.printf("Payout = %,.2f%n",bigDecimal.percentageFloatReturnAmount(policyAmount,percentage));
+
+        double totalUsingFloat = policyAmount -
+                ((policyAmount * percentageFloat) * beneficiaries);
+
+        System.out.println(totalUsingFloat);
     }
 }
--- src/test/java/org/example/tests/ApplicationTests.java
+++ src/test/java/org/example/tests/ApplicationTests.java
@@ -3,6 +3,7 @@ package org.example.tests;
 
 import junit.framework.Assert;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ApplicationTests {
@@ -14,6 +15,7 @@ public class ApplicationTests {
 
     }
 
+    @DisplayName("Pagani Test")
     @Test
     public void tester(){
         Assert.assertEquals("Hello","Hello");

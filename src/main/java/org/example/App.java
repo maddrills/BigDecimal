34f3245d9afb40e95124fa26130eb6ddf0f15830
mathew_francis <mathewfvv@gmail.com>
2023-08-11 13:42:40 +0530

    Second commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
@@ -14,15 +17,48 @@ public class App
         double percentage = 1.0 / beneficiaries;
 
 
-        BigDecimal bigDecimal = new BigDecimal();
+        BigDecimalMyOwn bigDecimalMyOwn = new BigDecimalMyOwn();
 
-        System.out.printf("Payout = %,.2f%n", bigDecimal.percentageFloatReturnAmount(policyAmount,percentageFloat));
+        System.out.printf("Payout = %,.2f%n", bigDecimalMyOwn.percentageFloatReturnAmount(policyAmount,percentageFloat));
 
-        System.out.printf("Payout = %,.2f%n",bigDecimal.percentageFloatReturnAmount(policyAmount,percentage));
+        System.out.printf("Payout = %,.2f%n", bigDecimalMyOwn.percentageFloatReturnAmount(policyAmount,percentage));
 
         double totalUsingFloat = policyAmount -
                 ((policyAmount * percentageFloat) * beneficiaries);
 
-        System.out.println(totalUsingFloat);
+        System.out.println("float yield "+totalUsingFloat);
+
+        double total = policyAmount -
+                ((policyAmount * percentage) * beneficiaries);
+
+        System.out.println("total "+total);
+
+
+        // reason for using strings in big decimal
+        final String[] tests = {
+                "5.456",
+                "8",
+                "10000.000001",
+                ".123"
+        };
+
+        final BigDecimal[] bds = new BigDecimal[tests.length];
+        Arrays.setAll(bds,i -> new BigDecimal(tests[i]));
+
+        System.out.printf("%-14s %-15s %-8s %s%n",
+                "Value",
+                "Unscaled Value",
+                "Scale",
+                "Precision");
+
+        for(var bd : bds){
+
+            System.out.printf("%-15s %-15d %-8d %d %n",
+                    bd,
+                    bd.unscaledValue(),
+                    bd.scale(),
+                    bd.precision());
+
+        }
     }
 }
